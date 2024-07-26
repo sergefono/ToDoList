3bb996b7817f45bd678e3eee94a96bb839e8e73c
ares <sergefono>
2024-07-26 04:13:36 +0100

    Mise en place du switch entre listes

--- src/app/shared/directives/drag.directive.ts
+++ src/app/shared/directives/drag.directive.ts
@@ -17,20 +17,31 @@ export class DragDirective {
   public isIn = false;
 
   //récupérer l’index de l’élément que nous déplaçons avec le drag
-  @Input('index')
-  public index!: number;
+  @Input('itemIndex')
+  public itemIndex!: number;
+  @Input('listIndex')
+  public listIndex!: number;
 
   /*transmettre l’index de l’élément draggé et l’index de l’élément sur lequel 
   il est droppé à notre composant afin d’effectuer l’interversion de ces éléments. */
   @Output()
-  public switch: EventEmitter<{ srcIndex: number; dstIndex: number }> =
-    new EventEmitter();
+  public switch: EventEmitter<{
+    src: {
+      itemIndex: number;
+      listIndex: number;
+    };
+    dst: {
+      itemIndex: number;
+      listIndex: number;
+    };
+  }> = new EventEmitter();
 
   /*définir des données dragguées au cours d'une opération de drag 
   and drop afin de passer l’index entre les éléments de la liste*/
   @HostListener('dragstart', ['$event'])
   public dragStart($event: DragEvent) {
-    $event.dataTransfer?.setData('srcIndex', this.index.toString());
+    $event.dataTransfer?.setData('itemIndex', this.itemIndex.toString());
+    $event.dataTransfer?.setData('listIndex', this.listIndex.toString());
   }
 
   /*émis lorsque la souris passe au-dessus d’un élément 
@@ -56,8 +67,14 @@ export class DragDirective {
   public drop($event: DragEvent) {
     this.isIn = false;
     this.switch.emit({
-      srcIndex: Number($event.dataTransfer?.getData('srcIndex')),
-      dstIndex: this.index,
+      src: {
+        itemIndex: Number($event.dataTransfer?.getData('itemIndex')),
+        listIndex: Number($event.dataTransfer?.getData('listIndex')),
+      },
+      dst: {
+        itemIndex: this.itemIndex,
+        listIndex: this.listIndex,
+      },
     });
   }
 
